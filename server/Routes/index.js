ddf7e3ba86a6a1739b5b84f454238493e448c8e9
OrenLeung <oren.leung@gmail.com>
2020-02-15 03:09:01 -0500

    swagger options

--- server/Routes/index.js
+++ server/Routes/index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const swaggerJsdoc = require('swagger-jsdoc');
 const swaggerUi = require('swagger-ui-express');
+const options = require('../swagger/index.js');
 
 // App Modules
 const User = require('../Models/User');
@@ -63,35 +64,6 @@ router.get('/users', (req, res, next) => {
 	res.json({ userOne, userTwo });
 });
 
-// Swagger set up
-const options = {
-	swaggerDefinition: {
-		openapi: '3.0.0',
-		info: {
-			title: 'Time to document that Express API you built',
-			version: '1.0.0',
-			description: 'A test project to understand how easy it is to document and Express API',
-			license: {
-				name: 'MIT',
-				url: 'https://choosealicense.com/licenses/mit/'
-			},
-			contact: {
-				name: 'Swagger',
-				url: 'https://swagger.io',
-				email: 'Info@SmartBear.com'
-			}
-		},
-		servers: [
-			{
-				url: 'https://hackthevalley123456789.appspot.com/api/v1'
-			},
-			{
-				url: 'https://localhost:3000/api/v1'
-			}
-		]
-	},
-	apis: [ './Models/User.js', './Routes/index.js' ]
-};
 const specs = swaggerJsdoc(options);
 router.use('/docs', swaggerUi.serve);
 router.get('/docs', swaggerUi.setup(specs, { explorer: true }));
