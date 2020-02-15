b10e22c84f3a9e4e2f07f4486957b5a597649ebf
OrenLeung <oren.leung@gmail.com>
2020-02-15 13:00:32 -0500

    reviewmodel

--- server/Models/ReviewModel.js
+++ server/Models/ReviewModel.js
@@ -2,11 +2,11 @@
 const mongoose = require('mongoose');
 
 const ReviewSchema = mongoose.Schema({
-    rating: String,
-    comment: String,
-    userName: String,
-    service: String,
-    date: {type:Date, default:Date.now}
+	rating: String,
+	comment: String,
+	userName: String,
+	service: String,
+	date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Reviews", ReviewSchema);
+module.exports = mongoose.model('Reviews', ReviewSchema);
