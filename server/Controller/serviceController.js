b2a0ba123f42a355fdd07f828faf440ffd19cc1f
OrenLeung <oren.leung@gmail.com>
2020-02-15 11:32:25 -0500

    idk

--- server/Controller/serviceController.js
+++ server/Controller/serviceController.js
@@ -1,37 +1,37 @@
 Service = require('../Models/ServiceModel.js');
 
 exports.findAll = async function(req, res) {
-    try {
-        const results = await Service.find({});
-        res.json({
-            status: 'success',
-            message: 'Service retrieved successfully',
-            data: results
-        });
-    } catch (err) {
-        res.json(err);
-        throw err;
-    }
+	try {
+		const results = await Service.find({});
+		res.json({
+			status: 'success',
+			message: 'Service retrieved successfully',
+			data: results
+		});
+	} catch (err) {
+		res.json(err);
+		throw err;
+	}
 };
 
 exports.new = function(req, res) {
-    let service = Service();
-    service.userID = req.body.userID;
-    service.title = req.body.title;
-    service.description = req.body.description;
-    service.rating = req.body.rating;
-    service.category = req.body.category;
-    service.cost = req.body.cost;
+	let service = Service();
+	service.userID = req.body.userID;
+	service.title = req.body.title;
+	service.description = req.body.description;
+	service.rating = req.body.rating;
+	service.category = req.body.category;
+	service.cost = req.body.cost;
 
-    //saves in database
-    service.save(function(err) {
-        if (err) {
-            res.json(err);
-            throw err;
-        }
-        res.json({
-            message: 'New Service created!',
-            data: service
-        });
-    });
+	//saves in database
+	service.save(function(err) {
+		if (err) {
+			res.json(err);
+			throw err;
+		}
+		res.json({
+			message: 'New Service created!',
+			data: service
+		});
+	});
 };
--- server/Models/ServiceModel.js
+++ server/Models/ServiceModel.js
@@ -2,13 +2,13 @@
 const mongoose = require('mongoose');
 
 const ServiceSchema = mongoose.Schema({
-    userID: String,
-    title: String,
-    description: String,
-    rating: String,
-    category: String,
-    cost: String,
-    date: {type:Date, default:Date.now}
+	userID: String,
+	title: String,
+	description: String,
+	rating: String,
+	category: String,
+	cost: String,
+	date: { type: Date, default: Date.now }
 });
 
-module.exports = mongoose.model("Services", ServiceSchema);
+module.exports = mongoose.model('Services', ServiceSchema);
